b6e7f20f096292159cdb84450cd2993a03b1b4f8
Zaid Mahdi <136346815+zaidmahdidev@users.noreply.github.com>
2024-06-03 10:37:40 +0300

    Add files via upload

---
+++ my portfolio/js/style-switcher.js
@@ -0,0 +1,24 @@
+
+
+const styeSwitcherToggle =document.querySelector('.style-switcher-toggler');
+styeSwitcherToggle.addEventListener("click",()=>{
+  document.querySelector('.style-switcher').classList.toggle('open');
+});
+
+window.addEventListener("scroll",()=>{
+    if(document.querySelector(".style-switcher").classList.contains("open")){
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+})
+
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color){
+    alternateStyles.forEach((style)=>{
+        if(color === style.getAttribute("title")){
+            style.removeAttribute("disabled");
+        }
+        else{
+            style.setAttribute("disabled","true");
+        }
+    })
+}
